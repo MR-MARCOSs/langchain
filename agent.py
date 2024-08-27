e347facf0ec012afba6cb727384e4d9c89fbeb08
Marcos Eduardo <marcosdudu001@gmail.com>
2024-08-27 11:24:34 -0300

    new tests with agent.py

--- agent.py
+++ agent.py
@@ -16,5 +16,8 @@ prompt = hub.pull('hwchase17/openai-functions-agent')
 agent = create_openai_functions_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-agent_executor.invoke({'input':'Olá, tudo bem?'})
+#agent_executor.invoke({'input':'Olá, tudo bem?'})
+#agent_executor.invoke({'input':'Me recomende vídeos de propagandas de produtos de limpeza'})
+agent_executor.invoke({'input':'Qual valor médio de interesse pelo termo One Piece?'})
+
 
